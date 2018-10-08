49b53ab1b35ba1f31a19a198c8a0184e69355389
anonymouskane <sebastienlafontaine@outlook.fr>
2018-10-08 15:05:26 +0200

    maj

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,6 +1,13 @@
 import React, {Component} from 'react';
 
 class Welcome extends Component {
+    constructor() {
+        super();
+        this.state = {
+            count: 0
+        }
+    }
+
     render() {
         return (
             <h1>Bonjour {this.props.name}</h1>
