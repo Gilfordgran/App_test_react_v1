52e627e6936d0933af1ce5058ef764127f046a01
anonymouskane <sebastienlafontaine@outlook.fr>
2018-10-08 12:43:28 +0200

    maj

---
+++ src/Welcome.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class Welcome extends Component {
+    render() {
+        return (
+            <h1>Bonjour</h1>
+        );
+    }
+}
+
+export default Welcome;
