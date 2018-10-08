fdb0214a2b4048640c4e4c468e4ef6fee8d9d2ec
anonymouskane <sebastienlafontaine@outlook.fr>
2018-10-08 15:42:45 +0200

    ajout bt +1 / -1

--- src/Welcome.js
+++ src/Welcome.js
@@ -8,9 +8,28 @@ class Welcome extends Component {
         }
     }
 
+    // ajoute 1
+    addOne() {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+
+    // enlève 1
+    lessOne() {
+        this.setState({
+            count: this.state.count -1
+        });
+    }
+
     render() {
         return (
+            <div>
             <h1>Bonjour {this.props.name}</h1>
+            <p>Compteur : {this.state.count}</p>
+            <button onClick={() => this.addOne()}>+1</button>
+            <button onClick={() => this.lessOne()}>-1</button>
+            </div>
         );
     }
 }
